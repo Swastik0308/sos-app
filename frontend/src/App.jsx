6eac898d92b26586c7ab155c86c1710cf80cc121
Vivek-0427 <vivekgouda0427@gmail.com>
2026-03-10 17:17:04 +0530

    Added Frontend Part

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App h-screen flex flex-col items-center justify-center'>
+          <h1 className='text-4xl font-bold text-center mt-10'>Welcome to SOS App</h1>
+          <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-4xl mt-20 w-50'>SOS</button>
+      </div>
+    </>
+  )
+}
+
+export default App
